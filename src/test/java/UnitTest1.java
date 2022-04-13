010b3b203615aa4a867afa850a7bb11228d33d7b
bluekotte <Kurilo.Val@yandex.by>
2022-04-13 20:56:23 +0300

    com1

---
+++ src/test/java/UnitTest1.java
@@ -0,0 +1,12 @@
+import org.testng.annotations.Test;
+
+public class UnitTest1 extends BaseTest {
+    @Test
+    public void test() {
+        System.out.println("First unit Test...");
+    }
+    @Test
+    public void test1() {
+        System.out.println("Second unit Test...");
+    }
+}
