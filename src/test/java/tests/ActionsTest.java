6e1901ce5ad1b2193555c5ba0f0977715470498d
bluekotte <Kurilo.Val@yandex.by>
2022-05-11 18:46:09 +0300

    AdvancedActions_draft

--- src/test/java/tests/ActionsTest.java
+++ src/test/java/tests/ActionsTest.java
@@ -1,9 +1,11 @@
 package tests;
 
 import baseEntities.BaseTest;
+import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 import services.WaitsService;
 
@@ -34,11 +36,76 @@ public class ActionsTest extends BaseTest {
         WaitsService wait = new WaitsService(driver, Duration.ofSeconds(10));
 
         WebElement fileUploadPath = wait.waitForExists(By.id("file-upload"));
-        String pathToFile = ActionsTest.class.getClassLoader().getResource("download.jpeg").getPath();
+        String pathToFile = ActionsTest.class.getClassLoader().getResource("download.png").getPath();
         System.out.println(pathToFile);
         fileUploadPath.sendKeys(pathToFile);
         wait.waitForExists(By.id("file-submit")).submit();
 
         Thread.sleep(5000);
+
+        //переделать
+    }
+
+    @Test
+    public void contextMenuText() throws InterruptedException {
+        driver.get("http://the-internet.herokuapp.com/context_menu");
+        Actions actions = new Actions(driver);
+
+        WaitsService wait = new WaitsService(driver, Duration.ofSeconds(10));
+
+        WebElement box = wait.waitForExists(By.id("hot-spot"));
+        actions.moveToElement(box)
+                .contextClick()
+                .build()
+                .perform();
+
+        Alert alert = driver.switchTo().alert();
+        Assert.assertEquals(alert.getText(), "You selected a context menu");
+        alert.accept();
+
+        Thread.sleep(2000);
+    }
+
+    @Test
+    public void dynamicControlsTest() throws InterruptedException {
+        driver.get("http://the-internet.herokuapp.com/dynamic_controls");
+        Actions actions = new Actions(driver);
+
+        WaitsService wait = new WaitsService(driver, Duration.ofSeconds(10));
+
+        WebElement checkBox = wait.waitForExists(By.xpath("//div/input"));
+        actions.moveToElement(checkBox)
+                .click()
+                .build()
+                .perform();
+        WebElement removeButton = wait.waitForExists(By.xpath("//button[text()='Remove']"));
+        actions.moveToElement(removeButton)
+                .click()
+                .build()
+                .perform();
+
+        WebElement loading = wait.waitForExists(By.xpath("//div[text()='Wait for it... ']"));
+
+        Assert.assertTrue(loading.isDisplayed());
+        Assert.assertTrue(wait.waitForElementInvisible(loading));
+        Assert.assertTrue(wait.waitForElementInvisible(checkBox));
+        Assert.assertTrue(wait.waitForVisibilityLocatedBy(By.xpath("//form/p")).isDisplayed());
+
+
+        WebElement input = wait.waitForExists(By.xpath("//form/input"));
+        Assert.assertFalse(input.isEnabled());
+        actions.moveToElement(input)
+                .click()
+                .build()
+                .perform();
+
+        WebElement enableButton = wait.waitForExists(By.xpath("//*[text()='Enable']"));
+        enableButton.click();
+
+        Assert.assertTrue(wait.waitForVisibilityLocatedBy(By.xpath("//form/p")).isDisplayed());
+        Assert.assertTrue(input.isEnabled());
+
+
+
     }
 }
